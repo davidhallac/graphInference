3aaf68093b3a105384fc832a8d30baf4255d4845
David Hallac <david.hallac@gmail.com>
2015-12-11 13:12:43 -0800

    Trying one sample per reading

--- oneReadingOnly.py
+++ oneReadingOnly.py
@@ -17,7 +17,7 @@ timeShift = 999 #Number of steps till new covariance matrix appears
 eps = 1e-2
 #Optimization parameters
 alpha = 0.1 # Lasso parameter
-beta = 3 # Weight between basis nodes
+beta = 10 # Weight between basis nodes
 
 
 

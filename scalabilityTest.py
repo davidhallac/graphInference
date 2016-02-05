3b4a0f5ddde8387b8e88cd9be926d8f0a3a1b075
David Hallac <david.hallac@gmail.com>
2016-02-04 18:15:09 -0800

    Added Scalability test

--- scalabilityTest.py
+++ scalabilityTest.py
@@ -9,8 +9,8 @@ import matplotlib.pylab as pl
 import time
 # for size 10, alph = 0.1 beta = 0.6
 # Problem parameters
-size = 10
-timesteps = 10# size/2
+size = 5
+timesteps = 5# size/2
 samplesPerStep = 10#int(np.log2(size))
 timeShift = timesteps/3 #Number of steps till new covariance matrix appears
 eps = 1e-3

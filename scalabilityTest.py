40a38d600b19aaf76ac47eeea77fd37f7e519eeb
David Hallac <david.hallac@gmail.com>
2016-02-04 22:25:58 -0800

    Added Scalability test

--- scalabilityTest.py
+++ scalabilityTest.py
@@ -10,8 +10,8 @@ import matplotlib.pylab as pl
 import time
 # for size 10, alph = 0.1 beta = 0.6
 
-sizeList = [10,100,250,500,700]
-timeList = [50,100]
+sizeList = [500,700]#[10,100,250,500,700]
+timeList = [10,20,30,40,50]
 timingVals = np.zeros([__builtin__.len(sizeList), __builtin__.len(timeList)])
 
 for sizeTemp in range(__builtin__.len(sizeList)):

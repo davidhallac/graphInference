fdceab7c88fcf6a4f758a805b8354b879ce71d09
David Hallac <david.hallac@gmail.com>
2016-02-04 19:52:08 -0800

    Added Scalability test

--- scalabilityTest.py
+++ scalabilityTest.py
@@ -8,119 +8,132 @@ import matplotlib.pylab as pl
 #import matplotlib.animation as an
 import time
 # for size 10, alph = 0.1 beta = 0.6
+
+sizeList = [10,20,30]
+timeList = [10,20]
+timingVals = np.zeros(len(sizeList), len(timeList))
+
+for sizeTemp in range(len(sizeList)):
+    for timeTemp in range(len(timeList)):
+
+
 # Problem parameters
-size = 500
-timesteps = 10# size/2
-useCVX = False
+# size = 500
+# timesteps = 10# size/2
+        size = sizeList[sizeTemp]
+        timesteps = timeList[timeTemp]
+        useCVX = False
 
-samplesPerStep = 10#int(np.log2(size))
-timeShift = timesteps/3 #Number of steps till new covariance matrix appears
-eps = 1e-3
+        samplesPerStep = 10#int(np.log2(size))
+        timeShift = timesteps/3 #Number of steps till new covariance matrix appears
+        eps = 1e-3
 
-# Optimization parameters
-alpha = 0.1 # Lasso parameter
-beta = 0.6 # Weight between basis nodes
-#set_length = 1
-#alpha_set = np.linspace(0.1,10,set_length)
-#beta_set = np.linspace(3,10,set_length)
-FroError = []
-Score = []
+        # Optimization parameters
+        alpha = 0.1 # Lasso parameter
+        beta = 0.6 # Weight between basis nodes
+        #set_length = 1
+        #alpha_set = np.linspace(0.1,10,set_length)
+        #beta_set = np.linspace(3,10,set_length)
+        FroError = []
+        Score = []
 
-# Generate sparse, random, inverse covariance matrix (inverse of inverseCov is original covariance matrix)
-np.set_printoptions(suppress=True, precision = 3, threshold = 5)
-S_true = np.zeros((size,size))
-while (alg.det(S_true) <= 1e-2 ):
-    #print int(numpy.log2(size))*size
-    G6 = GenRndGnm(PUNGraph, size, int((size**2)*0.05))
-    #G6 = snap.GenRndGnm(snap.PUNGraph, 5, 5)
-    S_true = numpy.zeros((size,size))
-    for EI in G6.Edges():
-        S_true[EI.GetSrcNId(), EI.GetDstNId()] = 0.6 #  np.random.rand(1) #2*(numpy.random.rand(1)-0.5)
-    #    S[EI.GetSrcNId(), EI.GetDstNId()] =  2*(np.random.rand(1)-0.5)
-#        print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
-    #print S, S.max(axis = 1)
-    #S =  S + S.T + np.diag( S.max(axis = 1) )#S.max()*numpy.matrix(numpy.eye(size))
-    S_true =  S_true + S_true.T + S_true.max()*np.matrix(np.eye(size))
-#    print S_true
-#    print alg.det(S_true)
-Cov = alg.inv(S_true)
-S_true2 = numpy.zeros((size,size))
-while (alg.det(S_true2) <= 1e-2 ):
-    #print int(numpy.log2(size))*size
-    G6 = GenRndGnm(PUNGraph, size, int((size**2)*0.05))
-    #G6 = snap.GenRndGnm(snap.PUNGraph, 5, 5)
-    S_true2 = np.zeros((size,size))
-    for EI in G6.Edges():
-        S_true2[EI.GetSrcNId(), EI.GetDstNId()] = 0.6 #  np.random.rand(1) #2*(numpy.random.rand(1)-0.5)
-    #    S[EI.GetSrcNId(), EI.GetDstNId()] =  2*(np.random.rand(1)-0.5)
-#        print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
-    #print S, S.max(axis = 1)
-    #S =  S + S.T + np.diag( S.max(axis = 1) )#S.max()*numpy.matrix(numpy.eye(size))
-    S_true2 =  S_true2 + S_true2.T + S_true2.max()*np.matrix(np.eye(size))
-#    print S
-#    print alg.det(S_true2)
-Cov2 = alg.inv(S_true2)
-S_true3 = numpy.zeros((size,size))
-while (alg.det(S_true3) <= 1e-2 ):
-    #print int(numpy.log2(size))*size
-    G6 = GenRndGnm(PUNGraph, size, int((size**2)*0.05))
-    #G6 = snap.GenRndGnm(snap.PUNGraph, 5, 5)
-    S_true3 = np.zeros((size,size))
-    for EI in G6.Edges():
-        S_true3[EI.GetSrcNId(), EI.GetDstNId()] = 0.6 #  np.random.rand(1) #2*(numpy.random.rand(1)-0.5)
-    #    S[EI.GetSrcNId(), EI.GetDstNId()] =  2*(np.random.rand(1)-0.5)
-#        print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
-    #print S, S.max(axis = 1)
-    #S =  S + S.T + np.diag( S.max(axis = 1) )#S.max()*numpy.matrix(numpy.eye(size))
-    S_true3 =  S_true3 + S_true3.T + S_true3.max()*np.matrix(np.eye(size))
-#    print S
-#    print alg.det(S_true2)
-Cov3 = alg.inv(S_true3)
+        # Generate sparse, random, inverse covariance matrix (inverse of inverseCov is original covariance matrix)
+        np.set_printoptions(suppress=True, precision = 3, threshold = 5)
+        S_true = np.zeros((size,size))
+        while (alg.det(S_true) <= 1e-2 ):
+            #print int(numpy.log2(size))*size
+            G6 = GenRndGnm(PUNGraph, size, int((size**2)*0.05))
+            #G6 = snap.GenRndGnm(snap.PUNGraph, 5, 5)
+            S_true = numpy.zeros((size,size))
+            for EI in G6.Edges():
+                S_true[EI.GetSrcNId(), EI.GetDstNId()] = 0.6 #  np.random.rand(1) #2*(numpy.random.rand(1)-0.5)
+            #    S[EI.GetSrcNId(), EI.GetDstNId()] =  2*(np.random.rand(1)-0.5)
+        #        print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
+            #print S, S.max(axis = 1)
+            #S =  S + S.T + np.diag( S.max(axis = 1) )#S.max()*numpy.matrix(numpy.eye(size))
+            S_true =  S_true + S_true.T + S_true.max()*np.matrix(np.eye(size))
+        #    print S_true
+        #    print alg.det(S_true)
+        Cov = alg.inv(S_true)
+        S_true2 = numpy.zeros((size,size))
+        while (alg.det(S_true2) <= 1e-2 ):
+            #print int(numpy.log2(size))*size
+            G6 = GenRndGnm(PUNGraph, size, int((size**2)*0.05))
+            #G6 = snap.GenRndGnm(snap.PUNGraph, 5, 5)
+            S_true2 = np.zeros((size,size))
+            for EI in G6.Edges():
+                S_true2[EI.GetSrcNId(), EI.GetDstNId()] = 0.6 #  np.random.rand(1) #2*(numpy.random.rand(1)-0.5)
+            #    S[EI.GetSrcNId(), EI.GetDstNId()] =  2*(np.random.rand(1)-0.5)
+        #        print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
+            #print S, S.max(axis = 1)
+            #S =  S + S.T + np.diag( S.max(axis = 1) )#S.max()*numpy.matrix(numpy.eye(size))
+            S_true2 =  S_true2 + S_true2.T + S_true2.max()*np.matrix(np.eye(size))
+        #    print S
+        #    print alg.det(S_true2)
+        Cov2 = alg.inv(S_true2)
+        S_true3 = numpy.zeros((size,size))
+        while (alg.det(S_true3) <= 1e-2 ):
+            #print int(numpy.log2(size))*size
+            G6 = GenRndGnm(PUNGraph, size, int((size**2)*0.05))
+            #G6 = snap.GenRndGnm(snap.PUNGraph, 5, 5)
+            S_true3 = np.zeros((size,size))
+            for EI in G6.Edges():
+                S_true3[EI.GetSrcNId(), EI.GetDstNId()] = 0.6 #  np.random.rand(1) #2*(numpy.random.rand(1)-0.5)
+            #    S[EI.GetSrcNId(), EI.GetDstNId()] =  2*(np.random.rand(1)-0.5)
+        #        print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
+            #print S, S.max(axis = 1)
+            #S =  S + S.T + np.diag( S.max(axis = 1) )#S.max()*numpy.matrix(numpy.eye(size))
+            S_true3 =  S_true3 + S_true3.T + S_true3.max()*np.matrix(np.eye(size))
+        #    print S
+        #    print alg.det(S_true2)
+        Cov3 = alg.inv(S_true3)
 
-gvx = TGraphVX()
-for i in range(timesteps):
-	# Generate random samples
-	x_samples = 0
-	if (i < timeShift):
-		x_samples = np.random.multivariate_normal(np.zeros(size), Cov, samplesPerStep).T
-	elif (i < 2*timeShift):
-		x_samples = np.random.multivariate_normal(np.zeros(size), Cov2, samplesPerStep).T
-	else:
-		x_samples = np.random.multivariate_normal(np.zeros(size), Cov3, samplesPerStep).T
-	empCov = np.cov(x_samples)
- 	if (samplesPerStep == 1):
-	 	empCov = x_samples*x_samples.T
+        gvx = TGraphVX()
+        for i in range(timesteps):
+        	# Generate random samples
+        	x_samples = 0
+        	if (i < timeShift):
+        		x_samples = np.random.multivariate_normal(np.zeros(size), Cov, samplesPerStep).T
+        	elif (i < 2*timeShift):
+        		x_samples = np.random.multivariate_normal(np.zeros(size), Cov2, samplesPerStep).T
+        	else:
+        		x_samples = np.random.multivariate_normal(np.zeros(size), Cov3, samplesPerStep).T
+        	empCov = np.cov(x_samples)
+         	if (samplesPerStep == 1):
+        	 	empCov = x_samples*x_samples.T
 
-#	print empCov
-	#Add Node, edge to previous timestamp
-	n_id = i
-	S = semidefinite(size,name='S')
-	obj = -log_det(S) + trace(empCov*S) #+ alpha*norm(S,1)
-	gvx.AddNode(n_id, obj)
+        #	print empCov
+        	#Add Node, edge to previous timestamp
+        	n_id = i
+        	S = semidefinite(size,name='S')
+        	obj = -log_det(S) + trace(empCov*S) #+ alpha*norm(S,1)
+        	gvx.AddNode(n_id, obj)
 
-	if (i > 0): #Add edge to previous timestamp
-		prev_Nid = n_id - 1
-		currVar = gvx.GetNodeVariables(n_id)
-		prevVar = gvx.GetNodeVariables(prev_Nid)
-		edge_obj = beta*norm(currVar['S'] - prevVar['S'],1) # one norm penalty function
-#		edge_obj = beta*norm(currVar['S'] - prevVar['S'],2) # two norm penalty function
-		gvx.AddEdge(n_id, prev_Nid, Objective=edge_obj)
+        	if (i > 0): #Add edge to previous timestamp
+        		prev_Nid = n_id - 1
+        		currVar = gvx.GetNodeVariables(n_id)
+        		prevVar = gvx.GetNodeVariables(prev_Nid)
+        		edge_obj = beta*norm(currVar['S'] - prevVar['S'],1) # one norm penalty function
+        #		edge_obj = beta*norm(currVar['S'] - prevVar['S'],2) # two norm penalty function
+        		gvx.AddEdge(n_id, prev_Nid, Objective=edge_obj)
 
-	#Add rake nodes, edges
-	gvx.AddNode(n_id + timesteps)
-	gvx.AddEdge(n_id, n_id+timesteps, Objective=alpha*norm(S,1))
+        	#Add rake nodes, edges
+        	gvx.AddNode(n_id + timesteps)
+        	gvx.AddEdge(n_id, n_id+timesteps, Objective=alpha*norm(S,1))
 
-print "Starting to solve:"
-t = time.time()
-if(useCVX):
-    gvx.Solve( UseADMM = False)
-else:
-    gvx.Solve(Rho = 0.5, Verbose=True)
+        print "Starting to solve:"
+        t = time.time()
+        if(useCVX):
+            gvx.Solve( UseADMM = False)
+        else:
+            gvx.Solve(Rho = 0.5, Verbose=True)
 
-end = time.time() - t
-print "SOLUTION TIME", end
+        end = time.time() - t
+        print "SOLUTION TIME", end
 
+        timingVals[sizeTemp][timeTemp] = end
 
+print timingVals
 
 
 

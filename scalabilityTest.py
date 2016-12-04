51875f177a9228a8f4b6aa1112529fc10704428c
David Hallac <david.hallac@gmail.com>
2016-12-03 18:18:54 -0800

    scalability tests

--- scalabilityTest.py
+++ scalabilityTest.py
@@ -1,5 +1,5 @@
-from inferGraphScalability import * #USECVX = TRUE, SCS
-# from snapvxScalability import * #USECVX = FALSE, Naive ADMM
+# from inferGraphScalability import * #USECVX = TRUE, SCS
+from snapvxScalability import * #USECVX = FALSE, Naive ADMM
 
 import numpy as np
 import numpy.linalg as alg
@@ -10,9 +10,9 @@ import matplotlib.pylab as pl
 import time
 
 # sizeList = [2,3,4,5,6,7,8,9,10,50,100,200,300,500,707,900,1000,1100]
-sizeList = [2,3,4,5,6,7,8,9,10,20,50]#,100,200]#,300,500]
+sizeList = [2,3,4,5,6,7,8,9,10,15,20]#,100,200]#,300,500]
 timeList = [10]
-useCVX = True
+useCVX = False
 
 np.random.seed(0)
 timingVals = np.zeros([__builtin__.len(sizeList), __builtin__.len(timeList)])

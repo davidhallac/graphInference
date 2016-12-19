ddf4232131ed446bc3e3838c894faa3fcaf424c9
youngsuk0723 <pys1987@gmail.com>
2016-12-18 23:33:09 -0800

    eliminate alpha and beta

--- plotTest.py
+++ plotTest.py
@@ -5,10 +5,10 @@ import scipy as spy
 
 import matplotlib as mpl
 import matplotlib.pylab as pl
-
-
-alpha = np.genfromtxt('alpha.csv', delimiter = ',' ) 
-beta = np.genfromtxt('beta.csv', delimiter = ',' ) 
+cov_mode = 2
+compare = True
+#alpha = np.genfromtxt('alpha.csv', delimiter = ',' ) 
+#beta = np.genfromtxt('beta.csv', delimiter = ',' ) 
 x = np.genfromtxt('x.csv', delimiter = ',' ) 
 e1 = np.genfromtxt('e1.csv', delimiter = ',' ) 
 e2 = np.genfromtxt('e2.csv', delimiter = ',' ) 
@@ -61,20 +61,20 @@ pl.rcParams.update({'font.size':14})
 #print '\nave_PN:', np.mean(e1_set[ind][:49]),  np.mean(e1_set[ind][51:]), np.mean(e1_set[ind])
 #print '\nave_PN:', np.mean(e2_set[ind][:49]),  np.mean(e2_set[ind][51:]), np.mean(e2_set[ind])
 #print '\nave_PN:', np.mean(e4_set[ind][:49]),  np.mean(e4_set[ind][51:]), np.mean(e4_set[ind])
-#if setLength == 1 and compare == True:
-#    pl.subplot(311)     
-#    pl.plot(x, e11, label = 'zero beta')
-#    pl.subplot(312)
-#    pl.plot(x, e21)
-#    pl.subplot(313)
-#    david2, = pl.semilogy(x, e41)
-#    pl.rc('legend',**{'fontsize':14})
-#    david3 = pl.legend([david1,david2],['TVGL','Baseline'], ncol=2, loc=7, bbox_to_anchor=(1,0.57), columnspacing=0.4) 
-#    david3.draw_frame(False)   
-#    print '\nave_Naive:', np.mean(e11)
-#    print '\nave_Naive:', np.mean(e21)
-#    print '\nave_Naive:', np.mean(e41)
-#Data_type = dataType + '%s'%(cov_mode) + '%s'%(samplesPerStep)
+if setLength == 1 and compare == True:
+    pl.subplot(311)     
+    pl.plot(x, e11, label = 'zero beta')
+    pl.subplot(312)
+    pl.plot(x, e21)
+    pl.subplot(313)
+    david2, = pl.semilogy(x, e41)
+    pl.rc('legend',**{'fontsize':14})
+    david3 = pl.legend([david1,david2],['TVGL','Baseline'], ncol=2, loc=7, bbox_to_anchor=(1,0.57), columnspacing=0.4) 
+    david3.draw_frame(False)   
+    print '\nave_Naive:', np.mean(e11)
+    print '\nave_Naive:', np.mean(e21)
+    print '\nave_Naive:', np.mean(e41)
+Data_type = dataType + '%s'%(cov_mode) + '%s'%(samplesPerStep)
 #pl.savefig(Data_type)
 #pl.savefig(Data_type+'.eps', format = 'eps', bbox_inches = 'tight', dpi = 1000)
 #pl.show()

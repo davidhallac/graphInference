1a9ab2e28e26b214c7aabad791b8808a418165b9
youngsuk0723 <pys1987@gmail.com>
2016-12-21 11:16:36 -0800

    add scrpt file

---
+++ scriptForTable.py
@@ -0,0 +1,8 @@
+import shlex, subprocess
+
+for a in [2, 5]:
+	for b in [1, 2, 5]:
+		print 'Now analyze', ' underlying ', a, ' type of covariance with ', b, ' penalty'
+		args = 'python2.7 MainSynGraph.py ' +  str(a) + ' ' + str(b) + ' > output'+str(a)+str(b)+'.txt'
+		p = subprocess.Popen(args,stdout=subprocess.PIPE, shell = True)
+		out,err = p.communicate()

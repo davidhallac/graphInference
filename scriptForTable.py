a521663bf527c222676dcad2c2e36b1e8a7da22f
youngsuk0723 <pys1987@gmail.com>
2017-01-12 01:24:11 -0800

    test for PN cov

--- scriptForTable.py
+++ scriptForTable.py
@@ -1,6 +1,6 @@
 import shlex, subprocess
 
-for a in [2, 5]:
+for a in [5]:
 	for b in [1, 2, 5]:
 		print 'Now analyze', ' underlying ', a, ' type of covariance with ', b, ' penalty'
 		args = 'python2.7 MainSynGraph.py ' +  str(a) + ' ' + str(b) + ' > output'+str(a)+str(b)+'.txt'
